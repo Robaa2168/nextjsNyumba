e74894b08ebd6031c1699abbfed688e3387f935b
ROBERT <robertlagat38@gmail.com>
2023-11-26 11:39:41 +0300

    repaired

--- components/Footer.js
+++ components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
     </section>
         <div className="container mx-auto p-4">
           <p className="text-center text-gray-500">
-            &copy; 2023 Airbnb, Inc. All rights reserved.
+            &copy; 2023 viewNyumba, Inc. All rights reserved.
           </p>
         </div>
       </footer>
